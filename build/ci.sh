403dec6e4326abafe1296dc32250c2655c5bf4a5
Atila Neves <atila.neves@gmail.com>
2026-08-27 13:49:00 +0200

    bottom-up now rt-bottom-up

--- build/ci.sh
+++ build/ci.sh
@@ -13,5 +13,5 @@ bin/ut
 
 build/bench.sh examples/ct-easy -w 0 -r 1
 build/bench.sh examples/ct-full -w 0 -r 1 -b ctfe -b dmd
-build/bench.sh examples/bottom-up -w 0 -r 1 -b dmd -b interpreter
+build/bench.sh examples/rt-bottom-up -w 0 -r 1 -b dmd -b interpreter
 build/bench.sh examples/rt -w 0 -r 1 -b dmd
