e26e30ed1a8553c53e7fcffa81d843fb904a7b80
Atila Neves <atila.neves@gmail.com>
2026-08-31 18:16:44 +0200

    Add rt-simple to benches

--- build/benches.sh
+++ build/benches.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 cd "$(git -C "$(dirname -- "${BASH_SOURCE[0]}")" rev-parse --show-toplevel)"
 
-for benchmark in ct-easy ct-full rt-perf rt; do
+for benchmark in ct-easy ct-full rt-perf rt-simple rt; do
     build/bench.sh "$benchmark" -w 0 -r 1
 done
