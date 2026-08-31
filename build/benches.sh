fb941cce0dc289382657e5361c9ca5f0dddba5e0
Atila Neves <atila.neves@gmail.com>
2026-08-31 18:19:31 +0200

    Fix benches

--- build/benches.sh
+++ build/benches.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 cd "$(git -C "$(dirname -- "${BASH_SOURCE[0]}")" rev-parse --show-toplevel)"
 
-for benchmark in ct-easy ct-full rt-perf rt-simple rt; do
+while IFS= read -r benchmark; do
     build/bench.sh "$benchmark" -w 0 -r 1
-done
+done < <(find examples -mindepth 1 -maxdepth 1 -type d -print | sort)
