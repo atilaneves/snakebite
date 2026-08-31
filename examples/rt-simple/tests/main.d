28f9d5e9858d327cecac83f7df66a281f369c91d
Atila Neves <atila.neves@gmail.com>
2026-08-31 18:10:50 +0200

    rt-simple prints error messages

--- examples/rt-simple/tests/main.d
+++ examples/rt-simple/tests/main.d
@@ -1,5 +1,6 @@
-import std.stdio: writeln;
+import std.stdio: writeln, stderr;
 import std.meta: AliasSeq;
+import std.array: join;
 
 static import rt_simple;
 static import rt_simple.cereal;
@@ -22,21 +23,25 @@ shared static this() {
 int main() {
     size_t total;
     size_t failed;
+    string[] messages;
     static foreach (module_; allModules)
-        runModuleTests!module_(total, failed);
+        runModuleTests!module_(total, failed, messages);
 
     writeln(total, " test(s) run, ", failed, " failed.");
+    if(messages.length) stderr.writeln(messages.join("------\n"));
     return failed == 0 ? 0 : 1;
 }
 
-private void runModuleTests(alias module_)(ref size_t total, ref size_t failed) {
+private void runModuleTests(alias module_)(ref size_t total, ref size_t failed, ref string[] messages) {
     alias tests = __traits(getUnitTests, module_);
     total += tests.length;
 
     static foreach (test; tests) {
         try
             test();
-        catch (Throwable)
+        catch (Throwable t) {
+            messages ~= t.msg;
             ++failed;
+        }
     }
 }
