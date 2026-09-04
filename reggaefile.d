99be4ae045d312c0f7d846d7c8379bcada5b2dfd
Atila Neves <atila.neves@gmail.com>
2026-09-04 13:34:52 +0100

    Share objects across Reggae targets

--- reggaefile.d
+++ reggaefile.d
@@ -8,6 +8,7 @@ import reggae.types: CompilerFlags;
 import std.algorithm: filter;
 import std.array: array;
 import std.process: environment, executeShell;
+import std.path: baseName;
 import std.string: chomp;
 
 string ldcPath() {
@@ -18,9 +19,11 @@ string ldcPath() {
     return result.output.chomp;
 }
 
-Target dubTarget(string compiler, string config,
-                 CompilerFlags flags = CompilerFlags()) {
+Target dubTarget(string compiler, string config, string objectSet,
+                 string output, CompilerFlags flags = CompilerFlags()) {
     auto buildOptions = options.dup;
+    buildOptions.dubObjsDir = "$builddir/.reggae/objs/bin/"
+        ~ objectSet ~ ".objs";
     buildOptions.dCompiler = compiler == "dmd"
         ? options.dCompiler
         : environment.get("LDC", ldcPath());
@@ -28,25 +31,34 @@ Target dubTarget(string compiler, string config,
         buildOptions.dubBuildType = "release";
 
     DubInfo info = configToDubInfo[config].dup;
-    info.options = buildOptions;
+    if (config == "acceptance-test")
+        info.packages[0].dflags ~= "-unittest";
     if (compiler != "dmd") {
         foreach (ref package_; info.packages)
             package_.dflags = package_.dflags
                 .filter!(a => a != "-debug" && a != "-g")
                 .array;
     }
+    info.options = buildOptions;
+    if (compiler == "dmd")
+        info.packages[0].importPaths = info.packages[0].importPaths
+            .filter!(a => a.baseName != "tests" && a.baseName != "acceptance")
+            .array ~ ["tests", "acceptance"];
     info.packages[0].targetPath = "bin";
+    info.packages[0].targetFileName = objectSet;
 
-    return dubBuild(buildOptions, info, CompilationMode.options, flags);
+    auto target = dubBuild(buildOptions, info, CompilationMode.options, flags);
+    target.rawOutputs[0] = "bin/" ~ output;
+    return target;
 }
 
 Build reggaeBuild() {
     auto build = Build(
-        dubTarget("dmd", "unittest"),
-        dubTarget("dmd", "acceptance-test"),
-        dubTarget("ldc2", "sb", CompilerFlags("-release", "-O")),
-        dubTarget("ldc2", "sb-repl", CompilerFlags("-release", "-O")),
-        dubTarget("ldc2", "bench", CompilerFlags("-release", "-O")),
+        dubTarget("dmd", "unittest", "unittest", "ut"),
+        dubTarget("dmd", "acceptance-test", "unittest", "at"),
+        dubTarget("ldc2", "sb", "release", "sb", CompilerFlags("-release", "-O")),
+        dubTarget("ldc2", "sb-repl", "release", "sb-repl", CompilerFlags("-release", "-O")),
+        dubTarget("ldc2", "bench", "release", "bench", CompilerFlags("-release", "-O")),
     );
     return build;
 }
