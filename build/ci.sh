b290b7c8da6fb877ce338c6fafbc1b08833a6e07
Atila Neves <atila.neves@gmail.com>
2026-09-07 15:33:36 +0100

    Run acceptance tests serially in CI

--- build/ci.sh
+++ build/ci.sh
@@ -11,7 +11,7 @@ bin/ut
 # in one `bin/at` call would put `at.bench.timing`'s compiler processes
 # on the same machine as the timing gate, which moves the gate's ratio
 # (see `acceptance/at/ffi/cost.d`).
-bin/at '~@timing'
+bin/at -s '~@timing'
 bin/at -s '@timing'
 build/test-repl.sh
 bin/sb -b bytecode examples/rt-simple
