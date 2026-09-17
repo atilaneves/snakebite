2631d620c8c3ecbcaa9626d984c859a0defafd29
Atila Neves <atila.neves@gmail.com>
2026-09-17 13:07:04 -0300

    Parenthesize cast template arguments in dependency images

--- source/snakebite/frontend/dependencyimage.d
+++ source/snakebite/frontend/dependencyimage.d
@@ -188,12 +188,12 @@ private extern(C++) class Collector : imported!"dmd.visitor".SemanticTimeTransit
                     }
                 }
                 import std.string: indexOf;
-                const spelling = instance.toChars.fromStringz;
+                const spelling = sourceSpelling(instance.toChars.fromStringz);
                 const arguments = spelling[spelling.indexOf('!') .. $].idup;
-                const scopeName = instance.tempdecl.parent.toPrettyChars(true).fromStringz;
+                const scopeName = sourceSpelling(instance.tempdecl.parent.toPrettyChars(true).fromStringz);
                 const reference = text("__traits(getOverloads, ", scopeName,
                     ", \"", instance.tempdecl.ident.toString, "\", true)");
-                const key = instance.toPrettyChars(true).fromStringz.idup;
+                const key = sourceSpelling(instance.toPrettyChars(true).fromStringz);
                 if (key !in _references)
                     _references[key] = Reference(reference, arguments);
                 _references[key].functions ~= function_;
@@ -242,4 +242,53 @@ private extern(C++) class Collector : imported!"dmd.visitor".SemanticTimeTransit
     override void visit(FuncExp expression) {
         expression.fd.accept(this);
     }
+}
+
+
+// DMD's diagnostic printer abbreviates single integer template arguments,
+// even when their type requires a cast. Such casts require parentheses in
+// source. Token boundaries keep nested instances and string arguments intact.
+private string sourceSpelling(in char[] spelling) {
+    import dmd.lexer: Lexer;
+    import dmd.globals: global;
+    import dmd.tokens: TOK;
+
+    const input = spelling ~ "\0";
+    scope lexer = new Lexer(null, input.ptr, 0, spelling.length,
+        false, false, global.errorSink, &global.compileEnv);
+    string result;
+    size_t copied;
+    lexer.nextToken;
+    while (lexer.token.value != TOK.endOfFile) {
+        if (lexer.token.value != TOK.not) {
+            lexer.nextToken;
+            continue;
+        }
+        lexer.nextToken;
+        if (lexer.token.value != TOK.cast_)
+            continue;
+        const start = lexer.token.ptr - input.ptr;
+        // Folded pointer and enum values can have more than one cast.
+        while (lexer.token.value == TOK.cast_) {
+            lexer.nextToken;
+            assert(lexer.token.value == TOK.leftParenthesis);
+            size_t depth;
+            do {
+                if (lexer.token.value == TOK.leftParenthesis)
+                    ++depth;
+                else if (lexer.token.value == TOK.rightParenthesis)
+                    --depth;
+                assert(lexer.token.value != TOK.endOfFile);
+                lexer.nextToken;
+            } while (depth);
+        }
+        if (lexer.token.value == TOK.min || lexer.token.value == TOK.add)
+            lexer.nextToken;
+        lexer.nextToken;
+        const end = lexer.token.ptr - input.ptr;
+        result ~= spelling[copied .. start] ~ "(" ~ spelling[start .. end] ~ ")";
+        copied = end;
+    }
+    result ~= spelling[copied .. $];
+    return result;
 }
--- tests/ut/ffi/symbol.d
+++ tests/ut/ffi/symbol.d
@@ -515,6 +515,44 @@ static foreach (backend; Matrix!(Omit!(Ctfe, Because.inexpressible,
 }
 
 
+static foreach (backend; Matrix!()) {
+    @("image.narrowTemplateArguments." ~ backend.stringof)
+    @Serial
+    unittest {
+        const sandbox = Sandbox();
+        enum moduleName = "image_narrow_" ~ backend.stringof;
+        sandbox.writeFile("deps/" ~ moduleName ~ ".d",
+            "module " ~ moduleName ~ ";\n" ~ q{
+                struct Selection(ushort value) { int member = value; }
+                int read(T)(T value) { return value.member; }
+                int number(short value)() { return value; }
+                int literal(string value)() { return value == "!cast(ushort)1u"; }
+            });
+        sandbox.writeFile("app/root_" ~ moduleName ~ ".d",
+            "module root_" ~ moduleName ~ ";\nimport " ~ moduleName ~ ";\n" ~ q{
+            int main() {
+                assert(read(Selection!1()) == 1);
+                assert(number!(-2)() == -2);
+                assert(literal!"!cast(ushort)1u"() == 1);
+                return 0;
+            }
+        });
+        const imports = [sandbox.inSandboxPath("deps")];
+        static if (is(backend == Native)) {
+            const executable = sandbox.inSandboxPath("test");
+            const result = execute([defaultCompiler, "-I" ~ imports[0],
+                sandbox.inSandboxPath("app/root_" ~ moduleName ~ ".d"), "-of=" ~ executable]);
+            result.status.shouldEqual(0, result.output);
+            execute([executable]).status.should == 0;
+        } else {
+            auto project = prepareProject(sandbox.inSandboxPath("app"), imports).project;
+            scope instance = new backend(project.program);
+            run(instance, project.program).should == 0;
+        }
+    }
+}
+
+
 @("image.templateArgumentImports")
 @Serial
 unittest {
