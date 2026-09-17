fbaf820e014346554059a690ecd33c70ca849888
Atila Neves <atila.neves@gmail.com>
2026-09-17 11:11:26 -0300

    benches.sh is opt-in

--- build/benches.sh
+++ build/benches.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
-while IFS= read -r benchmark; do
+for benchmark in ct-full rt-perf rt-cerealed-2; do
     bin/bench "$benchmark" -w 0 -r 1
-done < <(find examples -mindepth 1 -maxdepth 1 -type d -print | sort)
+done
