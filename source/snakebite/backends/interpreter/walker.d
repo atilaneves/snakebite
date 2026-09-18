6d244972bfffce299e63f05b4e341b5935aca34a
Atila Neves <atila.neves@gmail.com>
2026-09-18 06:16:13 -0300

    Use linked storage for imported shared variables

--- source/snakebite/backends/interpreter/walker.d
+++ source/snakebite/backends/interpreter/walker.d
@@ -227,7 +227,8 @@ private struct Shared {
     this(const Program program) {
         this.program = program;
         plans = PlanCache(program.dependencyImage);
-        nativeData = NativeData(&constantSymbolAddress, &classRuntimeInfo);
+        nativeData = NativeData(&constantSymbolAddress, &classRuntimeInfo,
+            &this.program.isRootOwned);
         runtimeTypes = RuntimeTypes(&this.program.isRootOwned,
             (name) => plans.resolve(name),
             &classRuntimeInfo,
