513b281d5ead20204c7ae7ad47b1959e7bb44c3d
Atila Neves <atila.neves@gmail.com>
2026-09-21 11:02:30 -0300

    Delete flaky test

--- tests/ut/bench/timing.d
+++ tests/ut/bench/timing.d
@@ -2,13 +2,9 @@ module ut.bench.timing;
 
 
 import bench.benchmark: benchmark;
-import bench.capture: captureStdout;
 import snakebite.backends: backendIdentity, Backends, Program;
-import snakebite.backends.bytecode: Bytecode;
-import snakebite.backends.interpreter: Interpreter;
 import snakebite.frontend.compiler: parseSnippets;
 import std.conv: text;
-import std.meta: AliasSeq;
 import ut;
 
 
@@ -35,32 +31,5 @@ unittest {
         if (report.hasCompile)
             (report.runTime.minimum >= report.compileTime.minimum)
                 .shouldBeTrue;
-    }}
-}
-
-
-@("benchmark.suppressesProgramStdout")
-unittest {
-    enum marker = "benchmark program output";
-    auto module_ = parseSnippets([
-        "module benchmarkOutput; import std.stdio; void main() { writeln(\""
-        ~ marker ~ "\"); }",
-    ])[0];
-    auto program = Program([module_]);
-
-    static foreach (BackendType; AliasSeq!(Bytecode, Interpreter)) {{
-        const result = captureStdout({
-            const report = benchmark(
-                BackendType.stringof,
-                backendIdentity!BackendType,
-                program,
-                0,
-                1,
-            );
-            report.passed.should == true;
-            return 0;
-        });
-
-        result.output.should == "";
     }}
 }
